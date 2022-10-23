74fb05fd9b56aafbf004fb5da9edab6fdeaf8b6a
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2022-10-22 20:11:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-alert("Welcome!")
-location.href = "https://zerius-learning.w3spaces.com"
+alert("Welcome!");
+var j = "";
+for(var i = 1; i < location.hash.length; i++) {
+  j += location.hash[i]
+}
+location.href = "https://zerius-learning.w3spaces.com/" + j;
