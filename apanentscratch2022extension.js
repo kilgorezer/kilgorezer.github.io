d387f652ea5cbe1a83c777c28a002b7c65075b56
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2022-10-25 13:34:02 -0700

    Update apanentscratch2022extension.js

--- apanentscratch2022extension.js
+++ apanentscratch2022extension.js
@@ -723,8 +723,8 @@ class MyUtilities {
       ],
       menus: {
         bools: {
-          items: ['true', 'false'],
-          acceptReporters: true
+          items: [true, false], // Fixed bug - @kilgorezer
+          acceptReporters: false // Modified by @kilgorezer
         },
         chars: {
           items: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
