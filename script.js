5c744e35f94419e7b93290280a53d080f780b794
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2022-11-29 18:27:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 alert("Welcome!");
 var a = document.getElementById("a");
 var j = "";
-if(location.hash != "") {
-  for(var i = 1; i < location.hash.length; i++) {
+if(location.hash != "" && location.hash[1] == "/") {
+  for(var i = 2; i < location.hash.length; i++) {
     j += location.hash[i];
   }
 }
