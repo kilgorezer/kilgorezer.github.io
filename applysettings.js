03997366805532f4ef6ed96f80b3fb04d3f4a068
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2023-06-17 22:29:06 -0700

    Update applysettings.js

--- applysettings.js
+++ applysettings.js
@@ -2,11 +2,8 @@ if(!localStorage.KZGITSETTINGS) {
   localStorage.KZGITSETTINGS = "{\"mode\": 0, \"selfredirect\": true}";
 }
 const controls = JSON.parse(localStorage.KZGITSETTINGS);
-if(!controls.mode) {
-  controls.mode = 0;
-}
 if(typeof controls.selfredirect != "boolean") {
-  controls.selfredirect = false
+  controls.selfredirect = true
 }
 if(controls.mode=1) {
   document.body.setAttribute("DarkMode", "");
