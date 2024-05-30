4028c2f319f90d357c2443b962fd38c60d5e860c
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2024-05-29 18:25:50 -0700

    Update applysettings.js

--- applysettings.js
+++ applysettings.js
@@ -1,12 +1,23 @@
 if(!localStorage.KZGITSETTINGS) {
-  localStorage.KZGITSETTINGS = "{\"mode\": 0, \"selfredirect\": true}";
+  localStorage.KZGITSETTINGS = "{\"mode\": 0, \"selfredirect\": true, \"experimentals\": false}";
 }
 const controls = JSON.parse(localStorage.KZGITSETTINGS);
 if(typeof controls.selfredirect != "boolean") {
   controls.selfredirect = true
 }
-if(controls.mode==1) {
-  document.body.setAttribute("DarkMode", "");
-}else if (controls.mode==0){
-  document.body.setAttribute("LightMode", "");
+if(typeof controls.experimentals != "boolean") {
+  controls.experimentals = false
+}
+switch (controls.mode) {
+  case 1:
+    document.body.setAttribute("Dark-Mode", "");
+  break;
+  case 0:
+    document.body.setAttribute("Light-Mode", "");
+  break;
+  case 2:
+    document.body.setAttribute("Minimal-Style", "")
+}
+if(controls.experimentals) {
+  document.body.setAttribute("experimental-stuff", "");
 }
